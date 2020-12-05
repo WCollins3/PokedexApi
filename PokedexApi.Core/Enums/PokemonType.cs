e292d66fa46cf4289e8c0148f9a8a49c19a1edc1
Bill Collins <wcollins113@gmail.com>
2020-12-05 17:33:43 -0500

    Add pokemon-related models to be used by the project
    Add Pokemon class and PokemonType enum to be used as the basic models
    of this project

---
+++ PokedexApi.Core/Enums/PokemonType.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokedexApi.Core.Enums
+{
+    public enum PokemonType
+    {
+        /// <summary>
+        ///     Normal type pokemon.
+        /// </summary>
+        Normal = 1,
+
+        /// <summary>
+        ///     Fire type pokemon.
+        /// </summary>
+        Fire = 2,
+
+        /// <summary>
+        ///     Water type pokemon.
+        /// </summary>
+        Water = 3,
+
+        /// <summary>
+        ///     Electric type pokemon.
+        /// </summary>
+        Electric = 4,
+
+        /// <summary>
+        ///     Grass type pokemon.
+        /// </summary>
+        Grass = 5,
+
+        /// <summary>
+        ///     Ice type pokemon.
+        /// </summary>
+        Ice = 6,
+
+        /// <summary>
+        ///     Fighting type pokemon.
+        /// </summary>
+        Fighting = 7,
+
+        /// <summary>
+        ///     Poison type pokemon.
+        /// </summary>
+        Poison = 8,
+
+        /// <summary>
+        ///     Ground type pokemon.
+        /// </summary>
+        Ground = 9,
+
+        /// <summary>
+        ///     Flying type pokemon.
+        /// </summary>
+        Flying = 10,
+
+        /// <summary>
+        ///     Psychic type pokemon.
+        /// </summary>
+        Psychic = 11,
+
+        /// <summary>
+        ///     Bug type pokemon.
+        /// </summary>
+        Bug = 12,
+
+        /// <summary>
+        ///     Rock type pokemon.
+        /// </summary>
+        Rock = 13,
+
+        /// <summary>
+        ///     Ghost type pokemon.
+        /// </summary>
+        Ghost = 14,
+
+        /// <summary>
+        ///     Dragon type pokemon.
+        /// </summary>
+        Dragon = 15,
+
+        /// <summary>
+        ///     Dark type pokemon.
+        /// </summary>
+        Dark = 16,
+
+        /// <summary>
+        ///     Steel type pokemon.
+        /// </summary>
+        Steel = 17,
+
+        /// <summary>
+        ///     Fairy type pokemon.
+        /// </summary>
+        Fairy = 18,
+    }
+}
---
+++ PokedexApi.Core/Models/Pokemon.cs
@@ -0,0 +1,33 @@
+﻿using PokedexApi.Core.Enums;
+using System.Collections.Generic;
+
+namespace PokedexApi.Core.Models
+{
+    public class Pokemon
+    {
+        /// <summary>
+        ///     Pokedex ID number of the <see cref="Pokemon"/>.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        ///     Name of the <see cref="Pokemon"/>.
+        /// </summary>
+        public int Name { get; set; }
+
+        /// <summary>
+        ///     Type(s) of the <see cref="Pokemon"/>.
+        /// </summary>
+        public IList<PokemonType> Types { get; set; }
+
+        /// <summary>
+        ///     Types of attacks that this <see cref="Pokemon"/> is weak to.
+        /// </summary>
+        public IList<PokemonType> Weaknesses { get; set; }
+
+        /// <summary>
+        ///     Pokedex description of the <see cref="Pokemon"/>.
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
